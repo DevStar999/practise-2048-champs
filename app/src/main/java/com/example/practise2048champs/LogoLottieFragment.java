c3f69fa8807522e3df65f5755aad2ccca6c39436
DevStar999 <wolverinerage99@gmail.com>
2022-08-29 01:53:00 +0530

    LogoLottieFragment is setup nice

---
+++ app/src/main/java/com/example/practise2048champs/LogoLottieFragment.java
@@ -0,0 +1,27 @@
+package com.example.practise2048champs;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class LogoLottieFragment extends Fragment {
+
+    public LogoLottieFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_logo_lottie, container, false);
+    }
+}
