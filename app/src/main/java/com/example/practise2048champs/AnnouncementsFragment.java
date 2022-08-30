03a0de6784c3e2db87f3ab9e7fece4c09bac28d5
DevStar999 <wolverinerage99@gmail.com>
2022-08-30 09:07:29 +0530

    Completed initial setup of the AnnouncementsFragment

---
+++ app/src/main/java/com/example/practise2048champs/AnnouncementsFragment.java
@@ -0,0 +1,28 @@
+package com.example.practise2048champs;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class AnnouncementsFragment extends Fragment {
+
+    public AnnouncementsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_announcements, container, false);
+    }
+}
