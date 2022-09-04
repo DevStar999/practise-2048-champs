15cc37bb082ed8c0d4b8a02c6b9c4a4ce2a9f168
DevStar999 <wolverinerage99@gmail.com>
2022-09-04 16:59:50 +0530

    Completed the implementation of a few more buttons in the SettingsFragment
    	The buttons namely include redirect to privacy policy web page & terms of service web page

--- app/src/main/java/com/example/practise2048champs/SettingsFragment.java
+++ app/src/main/java/com/example/practise2048champs/SettingsFragment.java
@@ -20,6 +20,8 @@ public class SettingsFragment extends Fragment {
     private LinearLayout facebookLinearLayout;
     private LinearLayout instagramLinearLayout;
     private LinearLayout twitterLinearLayout;
+    private LinearLayout privacyLinearLayout;
+    private LinearLayout termsLinearLayout;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -63,6 +65,22 @@ public class SettingsFragment extends Fragment {
                 startActivity(browserIntent);
             }
         });
+        privacyLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://nerdcoredevelopment.com/privacy-policy.html"));
+                startActivity(browserIntent);
+            }
+        });
+        termsLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://nerdcoredevelopment.com/terms-of-service.html"));
+                startActivity(browserIntent);
+            }
+        });
     }
 
     @Nullable
@@ -74,6 +92,8 @@ public class SettingsFragment extends Fragment {
         facebookLinearLayout = view.findViewById(R.id.facebook_linear_layout);
         instagramLinearLayout = view.findViewById(R.id.instagram_linear_layout);
         twitterLinearLayout = view.findViewById(R.id.twitter_linear_layout);
+        privacyLinearLayout = view.findViewById(R.id.privacy_policy_linear_layout);
+        termsLinearLayout = view.findViewById(R.id.terms_of_service_linear_layout);
 
         settingOnClickListeners();
 
