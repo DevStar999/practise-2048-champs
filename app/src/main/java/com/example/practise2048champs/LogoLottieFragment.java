832c2643095ed5f5546afe4698acb4d396673d65
DevStar999 <wolverinerage99@gmail.com>
2022-09-13 14:50:51 +0530

    Correcting minor bug related to LogoLottieFragment

--- app/src/main/java/com/example/practise2048champs/LogoLottieFragment.java
+++ app/src/main/java/com/example/practise2048champs/LogoLottieFragment.java
@@ -1,15 +1,19 @@
 package com.example.practise2048champs;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class LogoLottieFragment extends Fragment {
+    private Context context;
     private LottieAnimationView gamePreviewSpotlightLottie;
 
     public LogoLottieFragment() {
@@ -24,9 +28,16 @@ public class LogoLottieFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("com.nerdcoredevelopment.game2048champsfinal", Context.MODE_PRIVATE);
+
         View view = inflater.inflate(R.layout.fragment_logo_lottie, container, false);
 
         gamePreviewSpotlightLottie = view.findViewById(R.id.game_preview_spotlight_lottie);
+        if (sharedPreferences.getBoolean("toggleRotatingLight", true)) {
+            gamePreviewSpotlightLottie.playAnimation();
+        } else {
+            gamePreviewSpotlightLottie.pauseAnimation();
+        }
 
         return view;
     }
@@ -37,5 +48,11 @@ public class LogoLottieFragment extends Fragment {
         } else {
             gamePreviewSpotlightLottie.pauseAnimation();
         }
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
     }
 }
