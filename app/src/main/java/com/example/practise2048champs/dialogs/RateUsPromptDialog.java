beeec273a4e4b513075ec488321fa61a4fda1040
DevStar999 <wolverinerage99@gmail.com>
2022-09-25 00:04:27 +0530

    Settings the UI flags appropriately

--- app/src/main/java/com/example/practise2048champs/dialogs/RateUsPromptDialog.java
+++ app/src/main/java/com/example/practise2048champs/dialogs/RateUsPromptDialog.java
@@ -89,10 +89,11 @@ public class RateUsPromptDialog extends Dialog {
         super.show();
 
         // Set the dialog to immersive sticky mode
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         // Clear the not focusable flag from the window
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
