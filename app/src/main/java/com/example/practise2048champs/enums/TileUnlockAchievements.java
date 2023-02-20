3be356b9d4c5410897ce20655b1384e050dcd835
DevStar999 <wolverinerage99@gmail.com>
2023-02-20 07:36:42 +0530

    Slight improvements in the older achievements and added achievements related to the 'Undo' tool

--- app/src/main/java/com/example/practise2048champs/enums/TileUnlockAchievements.java
+++ app/src/main/java/com/example/practise2048champs/enums/TileUnlockAchievements.java
@@ -8,17 +8,17 @@ import lombok.Getter;
 public enum TileUnlockAchievements {
     TILE_UNLOCK_ACHIEVEMENT_LEVEL_1("Masterful", R.string.achievement_masterful, 1,
             2048L),
-    TILE_UNLOCK_ACHIEVEMENT_LEVEL_2("Brainiac", R.string.achievement_brainiac, 2,
+    TILE_UNLOCK_ACHIEVEMENT_LEVEL_2("Brainiac \uD83E\uDDE0", R.string.achievement_brainiac, 2,
             4096L),
-    TILE_UNLOCK_ACHIEVEMENT_LEVEL_3("Gifted", R.string.achievement_gifted, 3,
+    TILE_UNLOCK_ACHIEVEMENT_LEVEL_3("Gifted \uD83C\uDF81", R.string.achievement_gifted, 3,
             16384L),
-    TILE_UNLOCK_ACHIEVEMENT_LEVEL_4("Enigma", R.string.achievement_enigma, 4,
+    TILE_UNLOCK_ACHIEVEMENT_LEVEL_4("Enigma \uD83E\uDD39", R.string.achievement_enigma, 4,
             131072L);
 
     private String nameOfAchievement; // Name as per the name given in the Google Play Games project
     private int achievementStringResourceId; // Achievement Id as per the Id generated in the Google Play Games project
     private int levelOfAchievement; // Custom information for us to keep track of the level of achievement completed
-    private long achievementTileValue;
+    private long achievementTileValue; // The value of the achievement tile
 
     TileUnlockAchievements(String nameOfAchievement, int achievementStringResourceId, int levelOfAchievement,
                            long achievementTileValue) {
