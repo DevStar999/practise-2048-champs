94ab5e095dbe07188740a7211d0e2c46629019d4
DevStar999 <wolverinerage99@gmail.com>
2023-04-07 10:34:40 +0530

    Minor code refactoring

--- app/src/main/java/com/example/practise2048champs/GameActivity.java
+++ app/src/main/java/com/example/practise2048champs/GameActivity.java
@@ -1,14 +1,11 @@
 package com.example.practise2048champs;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.AppCompatTextView;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
